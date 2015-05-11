c4e462e9e168018616d4a7ea282aaa1b74293e25
Aswini S <ashumeow@live.com>
2015-05-11 15:20:34 +0530

    add solution

---
+++ Week-2/Quiz/Programming/solutions/mathlab/quiz2.m
@@ -0,0 +1,17 @@
+close all; 
+%clear all; 
+clc;
+load('c1p8.mat');
+
+sampling_period = 2; % in ms
+num_timesteps = 150;
+
+sta = compute_sta(stim, rho, num_timesteps);
+
+time = -sampling_period*(num_timesteps-1):sampling_period:0; %ms
+
+figure(1);
+plot(time, sta);
+xlabel('Time (ms)');
+ylabel('Stimulus');
+title('Spike-Triggered Average');
